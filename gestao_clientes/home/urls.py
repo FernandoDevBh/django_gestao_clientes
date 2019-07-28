7cf1657059e8688345d08bab6162dbdc28a170e4
FernandoDevBh <fsffernando2@gmail.com>
2019-07-28 10:46:57 -0300

    modulo de ClassBaseView

--- gestao_clientes/home/urls.py
+++ gestao_clientes/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, my_logout
+from .views import home, my_logout, HomePageView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', my_logout, name="logout"),
+    path('home2/', HomePageView.as_view()),
 ]
